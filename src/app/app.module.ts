4aa88577b8793c06d786fc05a327a7d5b6d14972
Adrian Saldivar <adriansaldivar18@gmail.com>
2018-03-23 17:31:03 -0700

    basecount, button containers,  input output, changed, bootstrap

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,7 @@ import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { CoreComponent } from './core/core.component';
 import { UserinputComponent } from './userinput/userinput.component';
+import { MobileComponent } from './mobile/mobile.component';
 
 
 @NgModule({
@@ -13,6 +14,7 @@ import { UserinputComponent } from './userinput/userinput.component';
     AppComponent,
     CoreComponent,
     UserinputComponent,
+    MobileComponent,
   ],
   imports: [
     BrowserModule,
